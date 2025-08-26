a2a486a72a8c50c81abf650353443b61a099120a
Enric Pou <enricpou@gmail.com>
2025-08-26 13:13:07 +0200

    Fix csv column names

--- workflow/scripts/split_nc_to_csv.py
+++ workflow/scripts/split_nc_to_csv.py
@@ -1,4 +1,5 @@
 import argparse
+import h3
 import xarray as xr
 
 try:
@@ -9,15 +10,23 @@ except NameError:
     output_dir_default = None
 
 def main(input_file, output_dir):
-    ds = xr.open_dataset(input_file)
+    ds = xr.open_dataset(input_file).assign_coords(
+        h3_index=xr.apply_ufunc(
+            h3.int_to_str,
+            ds['cell_ids'],
+            vectorize=True,
+            dask='parallelized',
+            output_dtypes=[str]
+        )
+    ).swap_dims(
+        {'cell_ids': 'h3_index'}
+    ).drop_vars('cell_ids').rename(
+        {'rm': 'value'}
+    )
 
     for date in ds.date.dt.date.values:
-        df = ds.sel(date=ds.date.dt.date == date).rename({
-            'cell_ids': 'h3_index',
-            'rm': 'value'
-        }).to_dataframe().reset_index()
-        df.drop(columns=['date'], inplace=True)
-        df.to_csv(f"{output_dir}/{date}.csv", index=False)
+        df = ds.sel(date=ds.date.dt.date == date).drop_vars('date').to_dataframe()
+        df.to_csv(f"{output_dir}/{date}.csv")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
